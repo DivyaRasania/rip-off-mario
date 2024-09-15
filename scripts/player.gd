d8a1c5974e17d40f8e933f42b60966c28bdb6281
DivyaRasania <rasaniadivya1@gmail.com>
2024-09-15 01:20:53 -0400

    Added coins

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 120.0
+const JUMP_VELOCITY = -350.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
