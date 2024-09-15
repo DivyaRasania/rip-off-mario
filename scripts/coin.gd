a43541f83f8be9e43f838c1e2672efb2af938c04
DivyaRasania <rasaniadivya1@gmail.com>
2024-09-15 01:49:20 -0400

    Minor changes to functions parameter

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,4 +1,4 @@
 extends Area2D
 
-func _on_body_entered(body: Node2D):
+func _on_body_entered(body):
 	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const SPEED = 120.0
 const JUMP_VELOCITY = -350.0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
