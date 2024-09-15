2f5845cf8bab240aa9a5c107c7958234e9e04f59
DivyaRasania <rasaniadivya1@gmail.com>
2024-09-15 00:57:35 -0400

    Modified player speed and jumping key

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 130.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
 func _physics_process(delta: float) -> void:
@@ -9,7 +9,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
