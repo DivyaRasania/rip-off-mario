693727aaf605b72cfb906fa50834064a2160f546
DivyaRasania <rasaniadivya1@gmail.com>
2024-09-16 16:23:29 -0400

    Modified death animations

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,7 +3,10 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body):
+	Engine.time_scale = 0.25
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 func _on_timer_timeout():
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
